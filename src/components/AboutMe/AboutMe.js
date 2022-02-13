f55b4e640039aeef8bc3c7048c78cc0ba7f95675
Jack Ha <jackha@gmail.com>
2022-02-13 15:48:13 +0100

    Added margins for text

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -3,9 +3,9 @@ import React from 'react'
 const aboutMe = () => {
   return (
     <React.Fragment>
-        <h3 id='over-mij'>Over mij</h3>
-        <p>Vanaf 2022 ontwerp en bouw ik meubels voor klanten. Het is mijn passie om van niets tot een concept tot een product te komen. De combinatie van mijn hoofd en mijn handen gebruiken maakt het zo'n mooi vak.</p>
-        <p>Ik ben een software ontwikkelaar met meer dan 15 jaar professionele ervaring. Daarnaast ontwerp en bouw ik al meer dan 20 jaar meubels. Graag combineer ik mijn kennis van 3d ontwerpen en de werkelijke uitvoering om tot mooie resultaten te komen.</p>
+        <h3 id='over-mij' className='site-contents'>Over mij</h3>
+        <p className='site-contents'>Vanaf 2022 ontwerp en bouw ik meubels voor klanten. Het is mijn passie om van niets tot een concept tot een product te komen. De combinatie van mijn hoofd en mijn handen gebruiken maakt het zo'n mooi vak.</p>
+        <p className='site-contents'>Ik ben een software ontwikkelaar met meer dan 15 jaar professionele ervaring. Daarnaast ontwerp en bouw ik al meer dan 20 jaar meubels. Graag combineer ik mijn kennis van 3d ontwerpen en de werkelijke uitvoering om tot mooie resultaten te komen.</p>
     </React.Fragment>
   )
 }
--- src/components/CustomFurniture/CustomFurniture.js
+++ src/components/CustomFurniture/CustomFurniture.js
@@ -3,8 +3,8 @@ import React from 'react'
 const customFurniture = () => {
   return (
     <React.Fragment>
-      <h3 id='meubels-op-maat'>Meubels op maat</h3>
-      <p>Laten we samen naar je wensen kijken. Op basis daarvan kan ik een of meerdere ontwerpen maken en indien gewenst breng ik dat tot uitvoering.</p>
+      <h3 id='meubels-op-maat' className='site-contents'>Meubels op maat</h3>
+      <p className='site-contents'>Laten we samen naar je wensen kijken. Op basis daarvan kan ik een of meerdere ontwerpen maken en indien gewenst breng ik dat tot uitvoering.</p>
     </React.Fragment>
   )
 }
