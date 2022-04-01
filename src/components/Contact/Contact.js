71e923ba1e7db6e72b556518ce8028e3af181be5
Jack Ha <jackha@gmail.com>
2022-04-01 21:44:28 +0200

    Added contact information

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -11,6 +11,18 @@ const contact = () => {
         <img className={classes.image} src={linkedin}></img>
         <a href='https://www.linkedin.com/in/jack-ha-0a96314/'>Jack Ha</a>
       </div>       
+      <div className={`${classes.row} site-contents` }>
+        <div className={classes.iconize}>@</div>
+        <a href='mailto:jackk@amplivibe.com'>jackk@amplivibe.com</a>
+      </div>       
+      <div className={`${classes.row} site-contents` }>
+        <div className={classes.iconize}>t</div>
+        <div>+31 6 55164119</div>
+      </div>       
+      <div className={`${classes.row} site-contents` }>
+        <div className={classes.iconize}>a</div>
+        <div>Lanslaan 6 3526XH Utrecht</div>
+      </div>       
     </React.Fragment>
   )
 }
