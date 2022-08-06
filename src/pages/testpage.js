a33225e3ffc35fdc60e1407f743f1873f32fb90c
Jack Ha <jackha@gmail.com>
2022-08-06 12:35:36 +0200

    Update test page

--- src/pages/testpage.js
+++ src/pages/testpage.js
@@ -1,17 +1,9 @@
 import Contact from "../components/Contact/Contact";
-import Navbar from "../components/Navbar/Navbar";
-
-import SmallLogo from "../assets/jackk-meubels-logo.png";
-
-
-function openContact()
-{
-  window.open("https://docs.google.com/forms/d/e/1FAIpQLSeG-PjEcmeTcSoKv5fKlr_GgnKeUHTcYFiGtivc4Un-LUIA_g/viewform?usp=sf_link")
-}
 
 function TestPage() {
   return (
     <div className="App">
+      Test page no lorem ipsum but Who's in bunker? Who's in bunker?
       <Contact />
     </div>
   );
