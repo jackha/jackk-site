f6e59acfe602d527f5f5b109d18cd48239756da9
Jack Ha <jackha@gmail.com>
2022-08-13 11:02:03 +0200

    More and better text for walnut cabinet

--- src/pages/walnutcabinet.js
+++ src/pages/walnutcabinet.js
@@ -28,20 +28,20 @@ function WalnutCabinetPage() {
         <h1 className='site-contents'>Art Deco kast</h1>
         <p className='site-contents'>Je komt het huis binnen. Recht voor je is een deur die naar een kamer gaat. Links van de deur is een hoekje en rechts is een wand van 2 meter lang, waar ook een radiator hangt. "Dat is de plek waar ik mijn spullen op wil bergen."</p>
         <p className='site-contents'>"Ik wil mijn schoenen kwijt kunnen en mijn jassen, mijn handschoenen, en de voordeur moet helemaal open kunnen." Mooi, de klant heeft een duidelijk beeld wat hij wil. Specifiek wordt Art Deco een aantal keer genoemd, waarna ik een paar staaltjes laat zien van massief hout. Heel snel wordt al bekend dat is het meubel iets van notenhout zal krijgen. Als contrast suggereer ik essen en de klant gaat akkoord.</p>
-        <p className='site-contents'>Materialen & afwerking: Notenhout (corpus), essen (inleg), afgewerkt in hardwax.</p>
+        <p className='site-contents'>Materialen & afwerking: Notenhout (corpus), essen (inleg), messing (hangers), afgewerkt in hardwax.</p>
       </div>
-      <ImageMagnify image_small={image_inlay_small} image={image_inlay} alt="De stukken essenhout voor de inleg wordt gefreesd" text="De stukken essenhout voor de inleg wordt gefreesd"/>
-      <ImageMagnify image_small={image_wip_inlay_small} image={image_wip_inlay} alt="Eens kijken of de stukken inleg ook passen..." text="Eens kijken of de stukken inleg ook passen..."/>
-      <ImageMagnify image_small={image1_small} image={image1} alt="Art Deco kast, onderste deel" text="Art Deco kast, onderste deel"/>
-      <ImageMagnify image_small={image2} image={image2} alt="Art Deco kast, linker deel" text="Art Deco kast, linker deel" ratio="0.66" />
-      <ImageMagnify image_small={image2b} image={image2b} alt="Art Deco kast, linker deel open" text="Art Deco kast, linker deel open" ratio="0.66" />
+      <ImageMagnify image_small={image_inlay_small} image={image_inlay} alt="De stukken essenhout voor de inleg wordt gefreesd">De stukken essenhout voor de inleg wordt gefreesd</ImageMagnify>
+      <ImageMagnify image_small={image_wip_inlay_small} image={image_wip_inlay} alt="Eens kijken of de stukken inleg ook passen...">Eens kijken of de stukken inleg ook passen...</ImageMagnify>
+      <ImageMagnify image_small={image1_small} image={image1} alt="Art Deco kast, onderste deel">Onderste deel van de kast, hier worden de schoenen opgeborgen. Bovenin zit ook een lade voor bijvoorbeeld handschoenen.</ImageMagnify>
+      <ImageMagnify image_small={image2} image={image2} alt="Art Deco kast, linker deel" ratio="0.66">Het linker deel in zijn geheel. De inleg van de bovenste helft vult de onderste helft goed aan.</ImageMagnify>
+      <ImageMagnify image_small={image2b} image={image2b} alt="Art Deco kast, linker deel open" ratio="0.66">Wanneer je de deurtjes openmaakt word je verrast door de lade, welke zijn eigen inleg ook heeft.</ImageMagnify>
 
-      <ImageMagnify image_small={image3} image={image3} alt="Art Deco kast, lade detail" text="Art Deco kast, lade detail" />
-      <ImageMagnify image_small={image4} image={image4} alt="Art Deco kast, greep en front detail" text="Art Deco kast, greep en front detail" ratio="0.66"/>
+      <ImageMagnify image_small={image3} image={image3} alt="Art Deco kast, lade detail">Alleen de lade zelf al vind ik prachtig geworden.</ImageMagnify>
+      <ImageMagnify image_small={image4} image={image4} alt="Art Deco kast, greep en front detail" ratio="0.66">De grepen mogen er ook zijn, die vullen mooi de inleg aan met zijn kwart cirkels.</ImageMagnify>
 
-      <ImageMagnify image_small={image5} image={image5} alt="Art Deco kast, rechter deel" text="Art Deco kast, rechter deel" ratio="0.66"/>
-      <ImageMagnify image_small={image5b} image={image5b} alt="Art Deco kast, radiator ombouw" text="Art Deco kast, radiator ombouw" />
-      <ImageMagnify image_small={image5c} image={image5c} alt="Art Deco kast, hangers" text="Art Deco kast, hangers" />
+      <ImageMagnify image_small={image5} image={image5} alt="Art Deco kast, rechter deel" ratio="0.66">Het rechter gedeelte is voor de jassen, met een radiatorombouw onderaan.</ImageMagnify>
+      <ImageMagnify image_small={image5b} image={image5b} alt="Art Deco kast, radiator ombouw">Er is ook zorg en aandacht besteed aan de radiatorombouw, zodat deze perfect past bij de rest van de kast. Zie bijvoorbeeld het "ovaaltje" aan de bovenkant.</ImageMagnify>
+      <ImageMagnify image_small={image5c} image={image5c} alt="Art Deco kast, hangers">De messing hangers zijn op zichzelf prachtig en combineren heel mooi met het notenhout.</ImageMagnify>
 
 
       <div className='site-section'>
