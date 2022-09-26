da0dce1cfbe45720a3f212f4a83754592b137435
Jack Ha <jackha@gmail.com>
2022-09-26 13:04:49 +0200

    Changed testimonials to full names

--- src/components/Testimonials/Testimonials.js
+++ src/components/Testimonials/Testimonials.js
@@ -9,11 +9,11 @@ const testimonials = () => {
         <h3 className='site-contents title'>Wat klanten vinden</h3>
         <div className={`${classes.row} ${classes.block} site-contents ` }>
           <div className={classes.testimonial}>“Wij zijn echt onder de indruk van je ontwerpen, ze zijn er mooier uitgekomen dan gedacht. Je hebt onze wensen goed verwerkt en echt over veel zaken nagedacht.”</div>
-          <div className={classes.testimonialname}>~ J. & S.</div>
+          <div className={classes.testimonialname}>~ Jackson & Shareen - Utrecht</div>
         </div>       
         <div className={`${classes.row} ${classes.block} site-contents` }>
           <div className={classes.testimonial}>“Jack heeft mij verrast met 2 bijzondere ontwerpen. Het eerste was precies waar ik om vroeg. Het 2e was 'out of the box' gedacht, wat meer ruimte creëerde en helemaal bij mij past. Ik werd er echt heel blij van!”</div>
-          <div className={classes.testimonialname}>~ L.</div>
+          <div className={classes.testimonialname}>~ Lisette - Amersfoort</div>
         </div>
       </div>
     </React.Fragment>
